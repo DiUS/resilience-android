2166859aacaf84b682e62d46a933fee99fdbed4e
Justin Berry <jtberry1@gmail.com>
2012-10-09 18:32:04 +1100

    Added simple test.

--- resilience/src/au/com/dius/resilience/activity/EditIncidentActivity.java
+++ resilience/src/au/com/dius/resilience/activity/EditIncidentActivity.java
@@ -1,8 +1,10 @@
 package au.com.dius.resilience.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import au.com.dius.resilience.R;
 
 public class EditIncidentActivity extends Activity {
@@ -17,5 +19,11 @@ public class EditIncidentActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_edit_incident, menu);
         return true;
+    }
+    
+    public void onSubmitClick(View button) {
+      EditText incidentNote = (EditText) findViewById(R.id.incident_note);
+      System.out.println("Note contains text: \"" + incidentNote.getText().toString()
+          + "\"");
     }
 }
--- resilienceTest/src/test/java/au/com/dius/resilience/activity/test/EditIncidentActivityTest.java
+++ resilienceTest/src/test/java/au/com/dius/resilience/activity/test/EditIncidentActivityTest.java
@@ -1,16 +1,37 @@
 package au.com.dius.resilience.activity.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
+import android.view.KeyEvent;
+import android.widget.Button;
+import android.widget.EditText;
+import au.com.dius.resilience.R;
 import au.com.dius.resilience.activity.EditIncidentActivity;
 
 public class EditIncidentActivityTest extends
     ActivityInstrumentationTestCase2<EditIncidentActivity> {
 
-  public EditIncidentActivityTest(Class<EditIncidentActivity> activityClass) {
-    super(activityClass);
+  private EditIncidentActivity activity;
+
+  public EditIncidentActivityTest() {
+    super("au.com.dius.resilience", EditIncidentActivity.class);
   }
-  
+
+  public void setUp() {
+    activity = getActivity();
+  }
+
   public void testSaveNoteToDB() {
     
+    EditText notes = (EditText) activity.findViewById(R.id.incident_note);
+    Button createButton = (Button) activity.findViewById(R.id.submit_incident);
+    
+    TouchUtils.clickView(this, notes);
+    this.sendKeys(KeyEvent.KEYCODE_F, KeyEvent.KEYCODE_I, KeyEvent.KEYCODE_R, KeyEvent.KEYCODE_E);
+    TouchUtils.clickView(this, notes);
+    
+    this.sendKeys(KeyEvent.KEYCODE_BACK);
+    
+    TouchUtils.clickView(this, createButton);
   }
 }
