c9278fa46e70a4946d92115d87f1ad5301f1f27b
Justin Berry <jtberry1@gmail.com>
2012-10-09 17:15:11 +1100

    Move test classes.

---
+++ resilienceTest/src/test/java/au/com/dius/resilience/activity/test/EditIncidentActivityTest.java
@@ -0,0 +1,16 @@
+package au.com.dius.resilience.activity.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import au.com.dius.resilience.activity.EditIncidentActivity;
+
+public class EditIncidentActivityTest extends
+    ActivityInstrumentationTestCase2<EditIncidentActivity> {
+
+  public EditIncidentActivityTest(Class<EditIncidentActivity> activityClass) {
+    super(activityClass);
+  }
+  
+  public void testSaveNoteToDB() {
+    
+  }
+}
---
+++ resilienceTest/src/test/java/au/com/dius/resilience/activity/test/ResilienceActivityTest.java
@@ -0,0 +1,26 @@
+package au.com.dius.resilience.activity.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import au.com.dius.resilience.activity.ResilienceActivity;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class au.com.dius.resilience.ResilienceActivityTest \
+ * au.com.dius.resilience.tests/android.test.InstrumentationTestRunner
+ */
+public class ResilienceActivityTest extends ActivityInstrumentationTestCase2<ResilienceActivity> {
+
+    public ResilienceActivityTest() {
+        super("au.com.dius.resilience", ResilienceActivity.class);
+    }
+
+    public void testThingsAreOk() {
+      
+      assertTrue(true);
+    }
+}
