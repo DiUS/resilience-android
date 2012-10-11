b759f3df377a49ac65602d18269a6c9c99669ca3
George Papas <george.papas@gmail.com>
2012-10-11 13:34:14 +1100

    Fixed merge issues

--- resilience/src/au/com/dius/resilience/fragment/IncidentListFragment.java
+++ resilience/src/au/com/dius/resilience/fragment/IncidentListFragment.java
@@ -4,11 +4,11 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import au.com.dius.resilience.R;
 import au.com.dius.resilience.adapter.ListViewAdapter;
 import au.com.dius.resilience.model.Incident;
+import au.com.dius.resilience.model.IncidentFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,23 +43,23 @@ public class IncidentListFragment extends ListFragment {
   private List<Incident> getIncidents() {
     List<Incident> incidents = new ArrayList<Incident>();
 
-    incidents.add(new Incident("Foo 1", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 2", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 3", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 4", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 5", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 6", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 7", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 8", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 9", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 10", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 11", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 12", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 13", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 14", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 15", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 16", System.currentTimeMillis(), "I'm a note"));
-    incidents.add(new Incident("Foo 17", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 1", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 2", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 3", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 4", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 5", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 6", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 7", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 8", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 9", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 10", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 11", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 12", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 13", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 14", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 15", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 16", System.currentTimeMillis(), "I'm a note"));
+    incidents.add(IncidentFactory.createIncident("Foo 17", System.currentTimeMillis(), "I'm a note"));
 
     return incidents;
   }
