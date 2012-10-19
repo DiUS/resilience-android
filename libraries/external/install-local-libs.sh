6d573a535920fb89f31a86c311304473816d55d0
George Papas <george.papas@gmail.com>
2012-10-19 22:43:39 +1100

    Added libraries folder

---
+++ libraries/external/install-local-libs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# In case of emergency, break glass.  Only here for first time checkouts when not at DiUS
+
+mvn install:install-file -Dfile=Parse.jar -DgroupId=XParse -DartifactId=Parse -Dversion=1.1.6 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=maps.jar -DgroupId=Xcom.google.android.maps -DartifactId=maps -Dversion=15r2 -Dpackaging=jar -DgeneratePom=true
