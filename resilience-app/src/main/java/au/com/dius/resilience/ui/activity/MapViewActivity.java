b69e56ddfa345b120a0e4f4ca916c39b6610af41
Justin Berry <jtberry1@gmail.com>
2012-10-23 10:05:41 +1100

    Add simple overlay.

--- resilience-app/src/main/java/au/com/dius/resilience/ui/activity/MapViewActivity.java
+++ resilience-app/src/main/java/au/com/dius/resilience/ui/activity/MapViewActivity.java
@@ -1,14 +1,19 @@
 package au.com.dius.resilience.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import au.com.dius.resilience.R;
+import au.com.dius.resilience.ui.map.ResilienceItemisedOverlay;
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 import roboguice.activity.RoboMapActivity;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author georgepapas
@@ -16,12 +21,24 @@ import java.util.ArrayList;
 @ContentView(R.layout.activity_map_view)
 public class MapViewActivity extends RoboMapActivity {
 
+  List<Overlay> mapOverlays;
+
   @InjectView(R.id.map_view)
   private MapView mapView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    mapOverlays = mapView.getOverlays();
+    Drawable drawable = this.getResources().getDrawable(R.drawable.create_incident);
+    ResilienceItemisedOverlay itemisedOverlay = new ResilienceItemisedOverlay(this, drawable);
+
+    GeoPoint geoPoint = new GeoPoint(19240000, -99120000);
+    OverlayItem overlayItem = new OverlayItem(geoPoint, "Mexico City, yay!", "I'm im Mexico city pals!");
+
+    itemisedOverlay.addOverlay(overlayItem);
+    mapOverlays.add(itemisedOverlay);
 
     mapView.setBuiltInZoomControls(true);
   }
---
+++ resilience-app/src/main/java/au/com/dius/resilience/ui/map/ResilienceItemisedOverlay.java
@@ -0,0 +1,11 @@
+package au.com.dius.resilience.ui.map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: justin
+ * Date: 22/10/12
+ * Time: 5:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResilienceItemisedOverlay {
+}
