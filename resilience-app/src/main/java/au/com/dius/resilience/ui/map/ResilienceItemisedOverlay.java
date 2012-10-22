1f8e2e72b60f52c946cf58825f552b62ef1313d0
Justin Berry <jtberry1@gmail.com>
2012-10-23 10:08:07 +1100

    Add itemised overlay.

--- resilience-app/src/main/java/au/com/dius/resilience/ui/map/ResilienceItemisedOverlay.java
+++ resilience-app/src/main/java/au/com/dius/resilience/ui/map/ResilienceItemisedOverlay.java
@@ -1,11 +1,46 @@
 package au.com.dius.resilience.ui.map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: justin
- * Date: 22/10/12
- * Time: 5:39 PM
- * To change this template use File | Settings | File Templates.
- */
-public class ResilienceItemisedOverlay {
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResilienceItemisedOverlay extends ItemizedOverlay {
+
+  private List<OverlayItem> overlays = new ArrayList<OverlayItem>();
+  private Context context;
+
+  public ResilienceItemisedOverlay(Context context, Drawable defaultMarker) {
+    super(boundCenterBottom(defaultMarker));
+    this.context = context;
+  }
+
+  public void addOverlay(OverlayItem overlayItem) {
+    overlays.add(overlayItem);
+    populate();
+  }
+
+  @Override
+  protected OverlayItem createItem(int i) {
+    return overlays.get(i);
+  }
+
+  @Override
+  public int size() {
+    return overlays.size();
+  }
+
+  @Override
+  protected boolean onTap(int index) {
+    OverlayItem item = overlays.get(index);
+    AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+    dialog.setTitle(item.getTitle());
+    dialog.setMessage(item.getSnippet());
+    dialog.show();
+    return true;
+  }
 }
