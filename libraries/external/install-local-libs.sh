0d4507cc87805cb9f98dd79b1b8395ca500400d3
Justin Berry <jtberry1@gmail.com>
2012-10-25 11:23:05 +1100

    Add external lib + script entry.

--- libraries/external/install-local-libs.sh
+++ libraries/external/install-local-libs.sh
@@ -4,3 +4,4 @@
 
 mvn install:install-file -Dfile=Parse.jar -DgroupId=XParse -DartifactId=Parse -Dversion=1.1.6 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=maps.jar -DgroupId=Xcom.google.android.maps -DartifactId=maps -Dversion=15r2 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=mapviewballoons.apklib -DgroupId=Xcom.readystatesoftware -DartifactId=mapviewballoons -Dversion=1.0-SNAPSHOT -Dpackaging=apklib -DgeneratePom=true
