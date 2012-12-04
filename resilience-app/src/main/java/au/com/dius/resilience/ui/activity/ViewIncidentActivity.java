520e63482a5105defb0c081a2b8a1d56e9b005c8
Thilina Ranaweera <tranaweera@gmail.com>
2012-12-04 22:20:43 +1100

    Modify activity incident view screen

--- resilience-app/src/main/java/au/com/dius/resilience/ui/activity/ViewIncidentActivity.java
+++ resilience-app/src/main/java/au/com/dius/resilience/ui/activity/ViewIncidentActivity.java
@@ -1,46 +1,44 @@
 package au.com.dius.resilience.ui.activity;
 
+import java.util.List;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.app.LoaderManager;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.Loader;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import au.com.dius.resilience.Constants;
 import au.com.dius.resilience.R;
-import au.com.dius.resilience.intent.Intents;
 import au.com.dius.resilience.loader.PhotoListLoader;
-import au.com.dius.resilience.model.Device;
 import au.com.dius.resilience.model.Incident;
 import au.com.dius.resilience.model.Photo;
 import au.com.dius.resilience.persistence.repository.Repository;
-import au.com.dius.resilience.receiver.IncidentTrackedBroadcastReceiver;
-import au.com.dius.resilience.receiver.IncidentUnTrackedBroadcastReceiver;
-import au.com.dius.resilience.service.TrackIncidentService;
-import au.com.dius.resilience.service.UntrackIncidentService;
 import au.com.dius.resilience.ui.Themer;
+
 import com.google.inject.Inject;
-import roboguice.activity.RoboActivity;
-import roboguice.inject.InjectResource;
-import roboguice.inject.InjectView;
-
-import java.util.List;
-
-import static au.com.dius.resilience.Constants.EXTRA_INCIDENT;
 
 public class ViewIncidentActivity extends RoboActivity implements LoaderManager.LoaderCallbacks<List<Photo>> {
 
   private static final String LOG_TAG = ViewIncidentActivity.class.getName();
 
-  @InjectView(R.id.incident_name)
+  @InjectView(R.id.activity_view_item_name)
   private TextView name;
+  
+  @InjectView(R.id.activity_view_item_time_reported)
+  private TextView time;
+  
+  @InjectView(R.id.activity_view_item_location)
+  private TextView location;
 
   @InjectView(R.id.incident_note)
   private TextView note;
@@ -56,37 +54,17 @@ public class ViewIncidentActivity extends RoboActivity implements LoaderManager.
 
   @Inject
   private Repository repository;
-
-  private IncidentTrackedBroadcastReceiver incidentTrackedBroadcastReceiver;
-  private IncidentUnTrackedBroadcastReceiver incidentUnTrackedBroadcastReceiver;
-
+  
   private Bitmap photoBitmap;
 
   private Incident incident;
-  private Menu menu;
-
-  @Override
-  protected void onResume() {
-    Log.d(LOG_TAG, "Registering broadcast receiver");
-    this.registerReceiver(
-            incidentTrackedBroadcastReceiver,
-            new IntentFilter(Intents.RESILIENCE_INCIDENT_TRACKED));
-
-    this.registerReceiver(
-            incidentUnTrackedBroadcastReceiver,
-            new IntentFilter(Intents.RESILIENCE_INCIDENT_UNTRACKED));
-
-    super.onRestart();
-  }
-
-  @Override
-  protected void onPause() {
-    Log.d(LOG_TAG, "Unregistering broadcast receiver");
-    this.unregisterReceiver(incidentTrackedBroadcastReceiver);
-    this.unregisterReceiver(incidentUnTrackedBroadcastReceiver);
-
-    super.onPause();
-  }
+ 
+  @InjectView(R.id.email_btn)
+  private Button emailButton;
+  
+  @InjectView(R.id.tweet_btn)
+  private Button tweetButton;
+  
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -94,40 +72,41 @@ public class ViewIncidentActivity extends RoboActivity implements LoaderManager.
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_view_incident);
 
-    incident = (Incident) getIntent().getSerializableExtra(EXTRA_INCIDENT);
+    incident = (Incident) getIntent().getSerializableExtra("incident");
     Log.d(LOG_TAG, "Incident retrieved with name " + incident.getName());
-
+    
+    
+    
     name.setText(incident.getName());
+    time.setText(DateUtils.getRelativeDateTimeString(
+            this,
+            incident.getDateCreated().longValue(),
+            DateUtils.SECOND_IN_MILLIS,
+            DateUtils.YEAR_IN_MILLIS, 0));
+    
+    location.setText("Long :"+incident.getPoint().getLatitude()+"  Lat :"+ incident.getPoint().getLatitude());
+    
     note.setText(incident.getNote());
 
     getLoaderManager().initLoader(PhotoListLoader.PHOTO_LIST_LOADER, null, this);
-    incidentTrackedBroadcastReceiver = new IncidentTrackedBroadcastReceiver(this);
-    incidentUnTrackedBroadcastReceiver = new IncidentUnTrackedBroadcastReceiver(this);
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case R.id.track_incident:
-        trackIncident();
-        break;
-      case R.id.untrack_incident:
-        unTrackIncident();
-        Log.d(LOG_TAG, "Untrack incident requested");
-        break;
-      default:
-        super.onOptionsItemSelected(item);
-    }
-
-    return true;
-  }
-
-  private void unTrackIncident() {
-    startService(UntrackIncidentService.createUnTrackingIntent(this, incident));
-  }
-
-  private void trackIncident() {
-    startService(TrackIncidentService.createTrackingIntent(this, incident));
+    
+    emailButton.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Log.i("email", "Click the email button");
+			
+		}
+	});
+    
+    tweetButton.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			Log.i("email", "Click the tweet button");
+			
+		}
+	});
   }
 
   public void onImageClick(View view) {
@@ -144,12 +123,10 @@ public class ViewIncidentActivity extends RoboActivity implements LoaderManager.
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    this.menu = menu;
     getMenuInflater().inflate(R.menu.activity_view_incident, menu);
-    getActionBar().setDisplayShowTitleEnabled(false);
-    updateTrackingMenu();
     return true;
   }
+
 
   @Override
   public Loader<List<Photo>> onCreateLoader(int i, Bundle bundle) {
@@ -185,37 +162,4 @@ public class ViewIncidentActivity extends RoboActivity implements LoaderManager.
     noImageLabel.setVisibility(View.VISIBLE);
   }
 
-
-  public void incidentTracked(String incidentId) {
-    Toast.makeText(this, "Incident being tracked", Toast.LENGTH_LONG).show();
-    if (incident.getId().equals(incidentId)) {
-      // HACK ALERT TODO, We need to use a loader to get a nice refresh/data load work
-      incident.addTracker(getCurrentUserId());
-    }
-    updateTrackingMenu();
-  }
-
-  private void updateTrackingMenu() {
-    if (incident.isTrackedBy(getCurrentUserId())) {
-      menu.getItem(0).setEnabled(false);
-      menu.getItem(1).setEnabled(true);
-    } else {
-      menu.getItem(0).setEnabled(true);
-      menu.getItem(1).setEnabled(false);
-    }
-  }
-
-  private String getCurrentUserId() {
-    return Device.getDeviceId(this);
-  }
-
-  public void incidentUnTracked(String incidentId) {
-    Toast.makeText(this, "Incident no longer being tracked", Toast.LENGTH_LONG).show();
-    if (incident.getId().equals(incidentId)) {
-      // HACK ALERT TODO, We need to use a loader to get a nice refresh/data load work
-      incident.removeTracker(getCurrentUserId());
-    }
-    updateTrackingMenu();
-
-  }
 }
