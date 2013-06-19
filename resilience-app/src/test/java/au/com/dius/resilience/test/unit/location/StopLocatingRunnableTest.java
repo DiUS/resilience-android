fdda37f4c560f66fb9e4e0ba7a2296c6672a1109
Justin Berry <jtberry1@gmail.com>
2013-06-19 21:08:45 +1000

    - Fix test.

--- resilience-app/src/test/java/au/com/dius/resilience/test/unit/location/StopLocatingRunnableTest.java
+++ resilience-app/src/test/java/au/com/dius/resilience/test/unit/location/StopLocatingRunnableTest.java
@@ -1,5 +1,7 @@
 package au.com.dius.resilience.test.unit.location;
 
+import android.app.Activity;
+import android.location.LocationManager;
 import au.com.dius.resilience.location.LocationBroadcaster;
 import au.com.dius.resilience.location.StopLocatingRunnable;
 import au.com.dius.resilience.test.unit.utils.ResilienceTestRunner;
@@ -8,19 +10,27 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import static junitx.util.PrivateAccessor.setField;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(ResilienceTestRunner.class)
 public class StopLocatingRunnableTest {
 
   private StopLocatingRunnable stopLocatingRunnable;
 
-  @Mock
   private LocationBroadcaster locationBroadcaster;
 
   @Before
   public void setup() {
+    locationBroadcaster = spy(new LocationBroadcaster());
+    TestHelper.setField(locationBroadcaster, "context", new Activity());
+    TestHelper.setField(locationBroadcaster, "locationManager", mock(LocationManager.class));
+
     stopLocatingRunnable = new StopLocatingRunnable(locationBroadcaster);
     TestHelper.setField(stopLocatingRunnable, "locationBroadcaster", locationBroadcaster);
   }
